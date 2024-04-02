8e296e64d420afa53898f045f40948b970e218d9
Sofiia <sonia.nosenko@gmail.com>
2024-04-02 17:37:11 +0200

    finish markup

--- app/app.js
+++ app/app.js
@@ -9,3 +9,13 @@ document
 document.querySelector("#close-btn").addEventListener("click", function () {
   document.querySelector(".mobile-menu").classList.remove("is-open");
 });
+
+document.querySelector(".hero-btn").addEventListener("click", function () {
+  document.querySelector(".backdrop").classList.toggle("is-open");
+});
+
+document
+  .querySelector("#modal-close-btn")
+  .addEventListener("click", function () {
+    document.querySelector(".backdrop").classList.remove("is-open");
+  });
